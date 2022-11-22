da716bd297c3991cb777039c9084569940de3f98
InderjitBedi <iinderjitbedi@gmail.com>
2022-11-22 17:55:34 -0500

    Heroku file added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require('cors')
