4f738e380da1652618c2235d407cf807d36d38de
InderjitBedi <iinderjitbedi@gmail.com>
2022-11-22 21:30:21 -0500

    added admin ui folder

--- app.js
+++ app.js
@@ -15,7 +15,12 @@ const FileRoutes = require("./routes/FileRoutes");
 dotenv.config();
 
 mongoose.connect("mongodb+srv://admin:admin@cluster0.kiw6oej.mongodb.net/admindb?retryWrites=true&w=majority");
-
+mongoose.connection.on("connected", () => {
+    console.log("Connected to DB")
+});
+mongoose.connection.on("error", err => {
+    console.log("DB connection failed: ", err)
+});
 app.use(express.json());
 
 var originsWhitelist = [
@@ -32,13 +37,19 @@ var corsOptions = {
 }
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //route middlewares
 app.use("/api/auth", AuthRoutes);
 app.use("/api/faq", FaqRoutes);
 // app.use("/api/file", FileRoutes);
+
 app.use(express.static(path.join(__dirname, 'FAQ')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
 
 
 app.listen(PORT, () => console.log(`Running server on port: ${PORT}`));
